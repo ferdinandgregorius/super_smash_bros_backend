615c0973e0a5048adcc9d44c3ee94ef7620a28b4
Gregorius Ferdinand <ferdinandgregorius@yahoo.com>
2021-11-11 11:38:18 +0700

    Added CREATE TABLE syntax for `item` table

--- super_smash_bros.sql
+++ super_smash_bros.sql
@@ -25,3 +25,11 @@ CREATE TABLE IF NOT EXISTS `super_smash_bros`.`character`(
     `admin_id` INT(7),
     FOREIGN KEY (`admin_id`) REFERENCES admin(admin_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+CREATE TABLE IF NOT EXISTS `super_smash_bros`.`item`(
+    `item_id` INT(7) PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(255),
+    `description` VARCHAR(255),
+    `admin_id` INT(7),
+    FOREIGN KEY (`admin_id`) REFERENCES admin(admin_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
