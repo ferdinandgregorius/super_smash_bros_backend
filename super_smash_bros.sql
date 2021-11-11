cc7b624284dd20ae3a8baae2bbbe10644577abb8
Gregorius Ferdinand <ferdinandgregorius@yahoo.com>
2021-11-11 11:36:25 +0700

    Added CREATE TABLE syntax for `character` table

--- super_smash_bros.sql
+++ super_smash_bros.sql
@@ -17,3 +17,11 @@ CREATE TABLE IF NOT EXISTS `super_smash_bros`.`admin`(
     FOREIGN KEY (`user_id`) REFERENCES user(user_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
+CREATE TABLE IF NOT EXISTS `super_smash_bros`.`character`(
+    `character_id` INT(7) PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(255),
+    `attributes` VARCHAR(255),
+    `description` VARCHAR(255),
+    `admin_id` INT(7),
+    FOREIGN KEY (`admin_id`) REFERENCES admin(admin_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
