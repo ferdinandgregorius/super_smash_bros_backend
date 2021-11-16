ac21d88d9bf6a831f3eba8a22e7d88ec61c0a99b
Gregorius Ferdinand <ferdinandgregorius@yahoo.com>
2021-11-16 14:29:35 +0700

    Added dao.js and strings.js

---
+++ strings.js
@@ -0,0 +1,17 @@
+//ERRORS
+export const NO_AFFECTED_ROWS="Records unchanged. Data has previously been updated or does not exist."
+export const SOMETHING_WENT_WRONG="Something Went Wrong In The Server. Contact Server Admin"
+export const WRONG_BODY_FORMAT="Wrong/Incomplete body format"
+
+export const ERROR_FOREIGN_KEY="There was a foreign key error. Please check the value of your fields and make sure the data exists"
+export const NO_SUCH_CONTENT="No Such Content"
+export const MISMATCH_OBJ_TYPE="Mismatch Object Type Passed"
+export const ERROR_DUPLICATE_ENTRY="Duplicate Entry Error"
+export const MAIN_ACCOUNT_EXISTS="There can only be 1 main account"
+export const NO_MAIN_AACOUNT="No main account."
+export const TRANSACTION_NOT_PENDING="Transaction has either been Approved or Rejected."
+export const DATA_IS_DELETED="Data has been deleted."
+export const ROLE_HAS_NO_ACCESS="This Role has no access to this feature."
+
+//CONSTRAINTS
+export const SUCCESS="SUCCESS"
